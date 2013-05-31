c340ddca155c0580ade11a89b7e40c1f130d00c7
charlie <charlie@10.239.89.38>
2013-05-31 17:53:57 +0200

    lo stesso di un secondo fa con una piccola modifica

---
+++ intro/Smarty-3.1.13/cache/75001e663669f3aa9a329f090ade3ea93bf4ec56.file.board.tpl.php
@@ -0,0 +1,94 @@
+<?php /* Smarty version Smarty-3.1.13, created on 2013-05-31 15:48:41
+         compiled from "Smarty-3.1.13\views\board.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2791851a8c659465837-19881868%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '75001e663669f3aa9a329f090ade3ea93bf4ec56' => 
+    array (
+      0 => 'Smarty-3.1.13\\views\\board.tpl',
+      1 => 1370013080,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2791851a8c659465837-19881868',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.13',
+  'unifunc' => 'content_51a8c6594b81e4_25881018',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51a8c6594b81e4_25881018')) {function content_51a8c6594b81e4_25881018($_smarty_tpl) {?><form id="board" action="default.php" method="post">
+
+<div id="board_table">
+    <div id = "first_row" class="board_row">
+        <div class="board_column empty_field"> &nbsp;</div>
+        <div class="board_column">Titolo</div>
+        <div class="board_column"><img id="arrowDown" alt="ss" src="imageintro/ArrowDownIcon.png"/><a class="noLinkStyle" title="ss" href="#">Data di Creazione</a></div>
+        <div id= "last_column" class="board_column">&nbsp;</div>
+    </div>
+    
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div> 
+   
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div> 
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div> 
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div> 
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div>     
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div> 
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div> 
+        <div class="board_row">
+            <div class="board_column empty_field">  <input type="checkbox"/> </div>
+            <div class="board_column">Documento ufficiale 23-4</div>
+            <div class="board_column">35/05/2013</div>
+            <div class="board_column last_column"><img alt="ss" src="imageintro/editIconOver.png"/><a href="#" title="modifica">Modifica</a><img alt="ss"  src="imageintro/cross-small.png"/><a href="#" title="elimina">Elimina</a></div>
+
+        </div> 
+
+    
+    <div id="select_all"> <img alt="ss" src="imageintro/arrow_ltr.png"/> <a href="#" title="seleziona/deseleziona">Seleziona Tutto / Deseleziona Tutto</a></div>
+</div>
+    
+</form><?php }} ?>
